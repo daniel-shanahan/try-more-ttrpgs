f9eec2ab576f2290002c6617a45f6ebb480573ec
Daniel Shanahan <daniel.patrick.shanahan@gmail.com>
2024-02-03 15:14:58 -0500

    Moved firebase to its own folder

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,8 @@
-import { initializeApp } from "firebase/app";
-import {
-  getFirestore,
-  collection,
-  getDocs,
-  Firestore,
-} from "firebase/firestore/lite";
+import { collection, getDocs, Firestore } from "firebase/firestore/lite";
+import { db } from "../firebase/firebase";
 import Image from "next/image";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyB7wsQaQ9lQzEAHvsVMuHUqNyshJj1zGAA",
-  authDomain: "try-more-ttrpgs.firebaseapp.com",
-  projectId: "try-more-ttrpgs",
-  storageBucket: "try-more-ttrpgs.appspot.com",
-  messagingSenderId: "406508966474",
-  appId: "1:406508966474:web:9559a4a73a1664a45f0cb3",
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-export default function Home() {
+export default async function Home() {
   async function getGames(db: Firestore) {
     const gamesCol = collection(db, "games");
     const gamesSnapshot = await getDocs(gamesCol);
@@ -28,26 +11,26 @@ export default function Home() {
     return gamesList;
   }
 
+  const games = await getGames(db);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <section>
         <h1 className="text-4xl">Games</h1>
         <div className="grid grid-cols-4 gap-4 mt-8">
-          {getGames(db).then((games) => {
-            return games.map((game) => {
-              return (
-                <div className="flex flex-col items-center justify-center">
-                  <Image
-                    src={game.imageUrl}
-                    alt={game.name}
-                    width={150}
-                    height={100}
-                  />
-                  <h2 className="text-xl font-bold">{game.name}</h2>
-                  <p className="">{game.description}</p>
-                </div>
-              );
-            });
+          {games.map((game) => {
+            return (
+              <div className="flex flex-col items-center justify-center">
+                <Image
+                  src={game.imageUrl}
+                  alt={game.name}
+                  width={150}
+                  height={100}
+                />
+                <h2 className="text-xl font-bold">{game.name}</h2>
+                <p className="">{game.description}</p>
+              </div>
+            );
           })}
         </div>
       </section>
---
+++ firebase/firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore/lite";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB7wsQaQ9lQzEAHvsVMuHUqNyshJj1zGAA",
+  authDomain: "try-more-ttrpgs.firebaseapp.com",
+  projectId: "try-more-ttrpgs",
+  storageBucket: "try-more-ttrpgs.appspot.com",
+  messagingSenderId: "406508966474",
+  appId: "1:406508966474:web:9559a4a73a1664a45f0cb3",
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
