e133ac88eb50251dde1c83ed526d5080c11b27b1
Daniel Shanahan <daniel.patrick.shanahan@gmail.com>
2024-02-20 10:59:57 -0500

    Added Modal component

---
+++ components/Modal.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { set } from "firebase/database";
+import { useState, useRef, useEffect } from "react";
+
+interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  children: React.ReactNode;
+}
+
+export default function Modal({ isOpen, onClose, children }: ModalProps) {
+  const [isModalOpen, setIsModalOpen] = useState(isOpen);
+  const modalRef = useRef<HTMLDialogElement | null>(null);
+
+  useEffect(() => {
+    setIsModalOpen(isOpen);
+  }, [isOpen]);
+
+  useEffect(() => {
+    const modalElement = modalRef.current;
+    if (!modalElement) return;
+
+    if (isModalOpen) {
+      modalElement.showModal();
+    } else {
+      modalElement.close();
+    }
+  }, [isModalOpen]);
+
+  const handleCloseModal = () => {
+    onClose();
+    setIsModalOpen(false);
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLDialogElement>) => {
+    if (e.key === "Escape") {
+      handleCloseModal();
+    }
+  };
+
+  return (
+    <dialog ref={modalRef} onKeyDown={handleKeyDown}>
+      <button onClick={handleCloseModal}>x</button>
+      {children}
+    </dialog>
+  );
+}
