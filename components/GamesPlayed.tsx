a47be7fc4889491b743d2993adeec1c563dfba16
Daniel Shanahan <daniel.patrick.shanahan@gmail.com>
2024-02-22 13:27:51 -0500

    Updated sort order of games that have been played

--- components/GamesPlayed.tsx
+++ components/GamesPlayed.tsx
@@ -4,17 +4,46 @@ import {
   Firestore,
   query,
   where,
+  orderBy,
+  CollectionReference,
+  DocumentData,
 } from "firebase/firestore";
 import { db } from "../firebase/firebase";
 import { Game } from "@/types/common.types";
 import GamesSection from "./GamesSection";
 
+async function getGamesPlayedWithDate(
+  gamesRef: CollectionReference<DocumentData, DocumentData>
+) {
+  const q = query(
+    gamesRef,
+    where("played", "==", true),
+    where("playedDate", "!=", ""),
+    orderBy("playedDate", "desc"),
+    orderBy("name")
+  );
+  const querySnapshot = await getDocs(q);
+  return querySnapshot.docs.map((doc) => doc.data()) as Game[];
+}
+
+async function getGamesPlayedWithoutDate(
+  gamesRef: CollectionReference<DocumentData, DocumentData>
+) {
+  const q = query(
+    gamesRef,
+    where("played", "==", true),
+    where("playedDate", "==", ""),
+    orderBy("name")
+  );
+  const querySnapshot = await getDocs(q);
+  return querySnapshot.docs.map((doc) => doc.data()) as Game[];
+}
+
 async function getGamesPlayed(db: Firestore) {
   const gamesRef = collection(db, "games");
-  const q = query(gamesRef, where("played", "==", true));
-  const querySnapshot = await getDocs(q);
-  const gamesList = querySnapshot.docs.map((doc) => doc.data()) as Game[];
-  return gamesList;
+  const gamesWithDate = await getGamesPlayedWithDate(gamesRef);
+  const gamesWithoutDate = await getGamesPlayedWithoutDate(gamesRef);
+  return [...gamesWithDate, ...gamesWithoutDate];
 }
 
 export default async function GamesToTry() {
